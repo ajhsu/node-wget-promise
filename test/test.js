a70856a556083ef214c6a82fb79ab34db7078d53
AJ Hsu <ajhsu@kkbox.com>
2017-07-30 16:28:32 +0800

    Rewrite testcase

--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
 'use strict';
 
-let wget = require('../build/wget');
-let expect = require('chai').expect;
+const fs = require('fs');
+const wget = require('../build/wget');
+const expect = require('chai').expect;
 
 describe('Download Tests', function() {
-  // with a proxy:
-  it('Should be able to download the NPM logo', function(done) {
+  this.timeout(30 * 1000);
+
+  it('Should download the NPM logo', function(done) {
     let download = wget.download(
       'https://www.npmjs.com/static/images/npm-logo.svg',
-      '/tmp/npm-logo.svg'
+      'npm-logo.svg'
     );
-    // @todo upgrade these tests to use a more consistent environment, with its own http server and files.
 
     download.on('error', function(err) {
       console.log(err);
       expect(err).to.be.null;
-      done();
     });
     download.on('start', function(fileSize) {
       expect(fileSize).to.be.a('string');
@@ -25,8 +25,39 @@ describe('Download Tests', function() {
     });
     download.on('end', function(output) {
       expect(output).to.equal('Finished writing to disk');
+      // Check if file existed
+      expect(fs.existsSync('npm-logo.svg')).to.be.true;
+      // Delete download file
+      fs.unlinkSync('npm-logo.svg');
       done();
-      process.exit();
+    });
+    download.on('progress', function(progress) {
+      expect(progress).to.be.above(0);
+    });
+  });
+
+  it('Should download the NPM logo without given output path', function(done) {
+    let download = wget.download(
+      'https://www.npmjs.com/static/images/npm-logo.svg?query=string'
+    );
+
+    download.on('error', function(err) {
+      console.log(err);
+      expect(err).to.be.null;
+    });
+    download.on('start', function(fileSize) {
+      expect(fileSize).to.be.a('string');
+      fileSize = Number(fileSize);
+      expect(fileSize).to.be.above(200);
+      expect(fileSize).to.be.below(500);
+    });
+    download.on('end', function(output) {
+      expect(output).to.equal('Finished writing to disk');
+      // Check if file existed
+      expect(fs.existsSync('npm-logo.svg')).to.be.true;
+      // Delete download file
+      fs.unlinkSync('npm-logo.svg');
+      done();
     });
     download.on('progress', function(progress) {
       expect(progress).to.be.above(0);
